a3d5b88a7f0a08b74eba346f3b20c736b03b9c8c
kira_penyaeva <kirsa.prikolnaja@gmail.com>
2023-03-03 16:32:20 +0300

    js and css work

---
+++ assets/app.js
@@ -0,0 +1,20 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
+import './plg/jQuery/jquery-3.5.0.slim.min'
+// import './plg/CountDown/jquery.countdown'
+// import './plg/CountDown/countdown'
+import './plg/form/jquery.form'
+import './plg/form/jquery.maskedinput.min'
+import './plg/range/ion.rangeSlider.min.js'
+import './plg/Slider/slick.min'
+import './js/scripts'
+
+// start the Stimulus application
+import './bootstrap';
