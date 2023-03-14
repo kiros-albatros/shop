3caadf200b95d2ea4f78b3096925c515fc2f7235
kira_penyaeva <kirsa.prikolnaja@gmail.com>
2023-03-14 17:08:29 +0300

    upload avatar

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use App\Form\ProfileEditFormType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProfileController extends AbstractController
 {
@@ -20,13 +22,41 @@ class ProfileController extends AbstractController
     }
 
     #[Route('/profile/edit', name: 'app_profile_edit')]
-    public function profile(Request $request,  ManagerRegistry $doctrine): Response
+    public function profile(Request $request,  ManagerRegistry $doctrine, SluggerInterface $slugger): Response
     {
         $user = $this->getUser();
         $form = $this->createForm(ProfileEditFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+
+            $file = $form->get('avatar')->getData();
+
+            if ($file) {
+                $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = '/uploads/avatars/'. $safeFilename.'.'.$file->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $file->move(
+                        $this->getParameter('avatars_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $user->setAvatar($newFilename);
+            } else {
+                dd('fuck');
+            }
+
+
             $entityManager = $doctrine->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
